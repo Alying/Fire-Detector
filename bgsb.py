c379dc8646bf7a1e754600198f642ca59f7ac21d
vanstorm9 <antlowhur@yahoo.com>
2018-09-08 04:19:17 -0400

    Still doing some work

--- bgsb.py
+++ bgsb.py
@@ -2,28 +2,75 @@ from helperFiles import brightSet
 import numpy as np
 import cv2
 
-#path = 'input-video/0-0.mp4'
-path = 'input-video/1-0.mp4'
+path = 'input-video/0-0.mp4'
 #path = 'input-video/0-1.mp4'
 
+
+#path = 'input-video/1-0.mp4'
+
 rad = 41
+cntAreaMin = 30
+#kernel = np.ones((3,3),np.uint8)
 
 cap = cv2.VideoCapture(path)
 fgbg = cv2.createBackgroundSubtractorMOG2()
-while(1):
+
+xMin = float('inf')
+yMin = float('inf')
+xMax = float('-inf')
+yMax = float('-inf')
+
+i = 0
+while True:
     ret, frame = cap.read()
 
     if frame is None:
 	break
 
     fgmask = fgbg.apply(frame)
+    #fgmask = cv2.erode(fgmask,kernel,iterations = 1)
 
-    brightFrame = brightSet.findMaxBrightness(frame,41)
+    mask, contours, hierarchy = cv2.findContours(fgmask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ 
+    if i%20 == 0:
+	xMin = float('inf')
+	yMin = float('inf')
+	xMax = float('-inf')
+	yMax = float('-inf')
+
+
+
+    for cnt in contours:
+	if cv2.contourArea(cnt) > cntAreaMin:
+		X,Y,W,H = cv2.boundingRect(cnt)
+
+		if X+W > xMax:
+			xMax = X+W
+		if Y+H > yMax:
+			yMax = Y+H
+
+		if X < xMin:
+			xMin = X
+		if Y < yMin:
+			yMin = Y
+
+		cv2.rectangle(frame,(X,Y),(X+W,Y+H),(0,255,0),2)
+
+
+    if len(contours) > 0 and xMin < xMax:
+    	cropFrame = frame.copy()[xMin:xMax,yMin:yMax]
+    else:
+	cropFrame = frame
+    #cropFrame = frame.copy()[0:xMax,0:yMax]
+   
+    brightFrame = brightSet.findMaxBrightness(frame,cropFrame,(xMin,yMin),41)
 
     cv2.imshow('frame',frame)
+    #cv2.imshow('frame crop',cropFrame)
     cv2.imshow('bg',fgmask)
     cv2.imshow('bright',brightFrame)
 
+    i += 1
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
--- helperFiles/brightSet.py
+++ helperFiles/brightSet.py
@@ -1,13 +1,20 @@
 import numpy as np
 import cv2
 
-def findMaxBrightness(image, rad):
+def findMaxBrightness(frame,image,minTup,rad):
+	xMin,yMin = minTup
 	res = image.copy()
+	res1 = frame.copy()
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 	gray = cv2.GaussianBlur(gray, (rad, rad), 0)
 	(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
 	#image = orig.copy()
-	cv2.circle(res, maxLoc, rad, (255, 0, 0), 2)
+	#cv2.circle(res, maxLoc, rad, (255, 0, 0), 2)
+	if not (xMin == float("inf") or yMin == float("inf")):
+        	newLoc =  (int(maxLoc[0]+xMin),int(maxLoc[1]+yMin))
+	else:
+		newLoc = maxLoc
+	cv2.circle(res1, newLoc, rad, (255, 0, 0), 2)
 
-	return res
+	return res1
