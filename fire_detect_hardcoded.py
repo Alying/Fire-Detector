5d6ac7a8e46d4e3621b128e5a64a597ee627d46a
Alying <alina.ying@gmail.com>
2018-09-08 00:18:32 -0700

    Added fire-detection message to user (hard-coded)

--- fire_detect_hardcoded.py
+++ fire_detect_hardcoded.py
@@ -1,17 +1,18 @@
-import cv2
 import numpy as np
- 
-#video_file = "video_1.mp4"
+import cv2
+
 path = input("Video path: ")
 
 cap = cv2.VideoCapture(path)
- 
-while True:
+
+while(cap.isOpened()):
     (grabbed, frame) = cap.read()
     if not grabbed:
         break
- 
-    blur = cv2.GaussianBlur(frame, (21, 21), 0)
+
+    #cv2.imshow('frame', frame)
+    #frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(frame, (21,21), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
  
     lower = [18, 50, 50]
@@ -19,17 +20,12 @@ while True:
     lower = np.array(lower, dtype="uint8")
     upper = np.array(upper, dtype="uint8")
     mask = cv2.inRange(hsv, lower, upper)
-     
+ 
     output = cv2.bitwise_and(frame, hsv, mask=mask)
-    no_red = cv2.countNonZero(mask)
-    cv2.imshow("output", output)
-    #print("output:", frame)
-    if int(no_red) > 20000:
-        print ('Fire detected')
-    #print(int(no_red))
-   #print("output:".format(mask))
+    cv2.imshow("output", output)    
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
-cv2.destroyAllWindows()
+
 cap.release()
+cv2.destroyAllWindows()
