9ef1bf7ecdc2fe9394e61dd8a8f99259bc9bff7e
Alying <alina.ying@gmail.com>
2018-09-07 23:12:01 -0700

    Convert to grayscale

--- fire_detect.py
+++ fire_detect.py
@@ -8,6 +8,11 @@ cap = cv2.VideoCapture(path)
 while(cap.isOpened()):
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
+    
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow('frame_gray', frame_gray)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
