587906f2a24d8ab214b69b715753c720e68607ff
Alying <alina.ying@gmail.com>
2018-09-08 20:10:53 -0700

    MongoDB Stitch

---
+++ templates/db.js
@@ -0,0 +1,40 @@
+const client = stitch.Stitch.initializeDefaultAppClient('firevideosdb-yrpwy');
+const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('VideosDB');
+
+client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => db.collection('VideosCollection').find().toArray());
+
+
+function addToDB(){
+    const id = document.getElementById("id").value;
+    const url = document.getElementById("url").value;
+    const videostream = document.getElementById("videostream").value;
+
+    const doc = { id, url, videostream }; 
+
+    //var dict = {"id" : document.getElementById("id").value, "url": document.getElementById("url").value,"videostream": document.getElementById("videostream").value}
+    client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => db.collection('VideosCollection').insertOne(doc));
+}
+function getAllPosts(){
+    client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => db.collection('VideosCollection').find());
+}
+
+function updateToDB() {
+     const vid_id = document.getElementById("id").value;
+    const vid_url = document.getElementById("url").value;
+    const vid_videostream = document.getElementById("videostream").value;
+
+    const doc = { vid_id, vid_url, vid_videostream }; 
+
+
+    //client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => 
+    db.collection('VideosDB').updateOne({id: vid_id}, {$set:{id: vid_id, url:vid_url, videostream:vid_videostream}}, {upsert:true})
+    .then(() => 
+    db.collection('VideosCollection').find().asArray()
+  ).then(docs => {
+      console.log("Found docs", docs)
+      console.log("[MongoDB Stitch] Connected to Stitch")
+  }).catch(err => {
+    console.error(err)
+  });
+
+}
