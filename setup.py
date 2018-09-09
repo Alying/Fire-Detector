1e6a112ff939d50a11d92ebe0bf63a0e61c814d0
Rohith Pentaparthy <penta@umich.edu>
2018-09-09 04:56:39 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name='',
+    packages=[''],
+    install_requires=[
+        'flask',
+        'requests',
+        'twilio',
+        'coverage',
+        'nose',
+        'pluggy',
+        'py',
+        'randomize',
+        'tox',
+        'docusign_esign'
+
+    ],
+)
